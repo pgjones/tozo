8b8834f253036512e675da8da67cad61019cc1e2
Phil Jones <pgjones@tozo.dev>
2022-07-22 14:52:51 +0100

    backend: Add libraries to manage users
    These libraries will allow users to authenticate and then manage their
    session.

--- backend/src/backend/run.py
+++ backend/src/backend/run.py
@@ -1,10 +1,13 @@
 from quart import Quart, ResponseReturnValue
+from quart_auth import AuthManager
 
 from backend.blueprints.control import blueprint as control_blueprint
 from backend.lib.api_error import APIError
 
 app = Quart(__name__)
 app.config.from_prefixed_env(prefix="TOZO")
+
+auth_manager = AuthManager(app)
 
 app.register_blueprint(control_blueprint)
 
