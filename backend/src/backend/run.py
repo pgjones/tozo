c168737c30fd0fde61184ba7c480cdbda68db3a1
Phil Jones <pgjones@tozo.dev>
2022-07-22 11:48:19 +0100

    backend: Configure the Quart app
    This allows the environment variables as loaded from either
    development.env or testing.env to configure the Quart app. This will
    allow for different configurations for dev, testing, and CI (via
    environment variables in CI).

--- backend/src/backend/run.py
+++ backend/src/backend/run.py
@@ -3,5 +3,6 @@ from quart import Quart
 from backend.blueprints.control import blueprint as control_blueprint
 
 app = Quart(__name__)
+app.config.from_prefixed_env(prefix="TOZO")
 
 app.register_blueprint(control_blueprint)
