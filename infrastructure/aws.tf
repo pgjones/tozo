f617bb8f9f9f776aa57c83dc1613511ef19f2da2
Phil Jones <pgjones@tozo.dev>
2022-07-24 16:02:36 +0100

    infrastructure: Add a database
    This will be used to store all the data in production in.

--- infrastructure/aws.tf
+++ infrastructure/aws.tf
@@ -11,3 +11,22 @@ provider "aws" {
   secret_key = var.aws_secret_key
   region     = "eu-west-2"
 }
+
+variable "db_password" {
+  sensitive = true
+}
+
+resource "aws_db_instance" "tozo" {
+  apply_immediately       = true
+  allocated_storage       = 20
+  backup_retention_period = 5
+  db_subnet_group_name    = aws_db_subnet_group.default.name
+  deletion_protection     = true
+  engine                  = "postgres"
+  engine_version          = "14"
+  instance_class          = "db.t3.micro"
+  db_name                 = "tozo"
+  username                = "tozo"
+  password                = var.db_password
+  vpc_security_group_ids  = [aws_security_group.database.id]
+}
--- infrastructure/aws_network.tf
+++ infrastructure/aws_network.tf
@@ -89,3 +89,25 @@ resource "aws_lb_target_group" "tozo" {
     type    = "lb_cookie"
   }
 }
+
+resource "aws_db_subnet_group" "default" {
+  subnet_ids = aws_subnet.private.*.id
+}
+
+resource "aws_security_group" "database" {
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "TCP"
+    cidr_blocks = aws_subnet.public.*.cidr_block
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = aws_subnet.public.*.cidr_block
+  }
+}
