3396b2f259e7c88d7575814420994d9b1e1fbf1b
Phil Jones <pgjones@tozo.dev>
2022-07-25 09:30:16 +0100

    infrastructure: Add DNS settings for Gandi webmail
    This allows emails to be received at tozo.dev addresses. This may not
    be useful for you and isn't in the book.

--- infrastructure/dns.tf
+++ infrastructure/dns.tf
@@ -25,3 +25,19 @@ resource "gandi_livedns_record" "tozo_dev_www" {
   ttl    = 3600
   values = ["tozo.dev."]
 }
+
+resource "gandi_livedns_record" "tozo_dev_MX" {
+  zone   = data.gandi_domain.tozo_dev.id
+  name   = "@"
+  type   = "MX"
+  ttl    = 10800
+  values = ["10 spool.mail.gandi.net.", "50 fb.mail.gandi.net."]
+}
+
+resource "gandi_livedns_record" "tozo_dev_SPF" {
+  zone   = data.gandi_domain.tozo_dev.id
+  name   = "@"
+  type   = "TXT"
+  ttl    = 10800
+  values = ["\"v=spf1 include:_mailcust.gandi.net ?all\""]
+}
